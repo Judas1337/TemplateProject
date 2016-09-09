4c781334c13504176abd6ba648cb04e8c7db3957
Grynet <nystad.fredrik@gmail.com>
2016-09-09 14:14:08 +0200

    Added Mocked test for GetProduct()

--- Temp.Tests/ProductsControllerTests.cs
+++ Temp.Tests/ProductsControllerTests.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Temp.Controllers;
 using Temp.Models;
+using Moq;
 
 namespace Temp.Tests
 {
@@ -23,9 +24,24 @@ namespace Temp.Tests
             Assert.AreEqual(expectedProduct.Price, actualProduct.Price, "Product Price");
         }
 
+        [TestMethod]
         public void GetProductOneMocked()
         {
-            
+            var expectedProduct = new Product() {Category = "Mock Category LogicHandled", Id = -1, Name = "Mock Name", Price = 10};
+
+            Mock<IProductRepository> productRepoMock = new Mock<IProductRepository>();
+            productRepoMock.Setup(productRepository => productRepository.GetProduct(1))
+                .Returns(expectedProduct);
+
+            ProductsController controller = new ProductsController(new ProductLogic(productRepoMock.Object));
+
+            var actualProduct = controller.GetProduct(1);
+
+            Assert.AreEqual(expectedProduct.Id, actualProduct.Id, "Product Id");
+            Assert.AreEqual(expectedProduct.Name, actualProduct.Name, "Product name");
+            Assert.AreEqual(expectedProduct.Category, actualProduct.Category, "Product Category");
+            Assert.AreEqual(expectedProduct.Price, actualProduct.Price, "Product Price");
+
         }
 
 
