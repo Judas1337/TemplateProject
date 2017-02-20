fbc777436844b26a931c0d086aae09ef22d8a01b
Grynet <nystad.fredrik@gmail.com>
2017-02-20 18:17:56 +0100

    Added ExceptionHandler & ExceptionLogger

---
+++ Temp/Utilities/ExceptionHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.ExceptionHandling;
+
+namespace Temp.Utilities
+{
+    public class ExceptionHandler : IExceptionHandler
+    {
+        public Task HandleAsync(ExceptionHandlerContext context, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Temp/Utilities/ExceptionLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+
+namespace Temp.Utilities
+{
+    public class ExceptionLogger : IExceptionLogger
+    {
+        public Task LogAsync(ExceptionLoggerContext context, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
