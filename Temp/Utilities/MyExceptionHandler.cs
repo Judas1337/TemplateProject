720bd4ae68fd105421b24e933eb090bf1443631f
Grynet <nystad.fredrik@gmail.com>
2017-02-22 10:41:57 +0100

    MyExceptionHandler: Added logic to put exception in different http status codes

--- Temp/Utilities/MyExceptionHandler.cs
+++ Temp/Utilities/MyExceptionHandler.cs
@@ -1,18 +1,50 @@
 ﻿
+using System;
+using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web.Http;
 using System.Web.Http.ExceptionHandling;
-
-
+using System.Web.Http.Results;
 
 
 namespace Temp.Utilities
 {
     public class MyExceptionHandler : ExceptionHandler
     {
-        public override Task HandleAsync(ExceptionHandlerContext context, CancellationToken cancellationToken)
+        public override void Handle(ExceptionHandlerContext context)
         {
-            return base.HandleAsync(context, cancellationToken);
+            HttpStatusCode statusCode;
+            if(context.Exception is ArgumentException) statusCode = HttpStatusCode.BadRequest;
+            else if (context.Exception is HttpResponseException) statusCode = (context.Exception as HttpResponseException).Response.StatusCode;
+            else statusCode = HttpStatusCode.InternalServerError;
+
+
+            context.Result = new TextPlainErrorResult
+            {
+                Request = context.Request,
+                Content = context.Exception.Message,
+                StatusCode = statusCode
+            };
+
+        }
+
+        private class TextPlainErrorResult : IHttpActionResult
+        {
+            public HttpRequestMessage Request { get; set; }
+
+            public string Content { get; set; }
+
+            public HttpStatusCode StatusCode { get; set; }
+
+            public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+            {
+                HttpResponseMessage response = new HttpResponseMessage(StatusCode);
+                response.Content = new StringContent(Content);
+                response.RequestMessage = Request;
+                return Task.FromResult(response);
+            }
         }
     }
 }
