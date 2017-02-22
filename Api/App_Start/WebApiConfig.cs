84b97af26c7a20b5d4846519729087b7fefd4a74
Grynet <nystad.fredrik@gmail.com>
2017-02-22 15:05:17 +0100

    WebApiConfig: removed convention based routing

--- Api/App_Start/WebApiConfig.cs
+++ Api/App_Start/WebApiConfig.cs
@@ -6,20 +6,8 @@ namespace WebApiTemplateProject.Api.App_Start
     {
         public static void Register(HttpConfiguration config)
         {
-            /*Enable attribute routing, eg attribute with routing info above methods
-             *Example: 
-             * [Route("{id:int})]
-             * public GetById(int id){}
-             */
+            //Routing will be correspond to the specified Route attribute for each method
             config.MapHttpAttributeRoutes();
-
-            //This routetemplate will respond to http://adress/api/controller/method/inputParameter
-            //Example  http://localhost:61837/api/products/getproduct/1
-            config.Routes.MapHttpRoute(
-                name: "Default",
-                routeTemplate: "api/{controller}/{action}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );           
         }
     }
 }
