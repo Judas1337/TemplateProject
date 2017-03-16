0d90bb8662238fd811489b7edb757494a30c22dc
Grynet <nystad.fredrik@gmail.com>
2017-03-16 17:13:53 +0100

    ExecutionContextValueProvider added

---
+++ Utilities/Concurrency/ExecutionContextValueProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiTemplateProject.Utilities.Concurrency
+{
+    public class ExecutionContextValueProvider : IExecutionContextValueProvider
+    {
+        private readonly string _correlationIdKey = "correlationId";
+
+        public string GetCorrelationId()
+        {
+            object correlationId = CallContext.LogicalGetData(_correlationIdKey);
+            return correlationId.ToString();
+        }
+
+        public void SetCorrelationId(string correlationId)
+        {
+            CallContext.LogicalSetData(_correlationIdKey, correlationId);
+        }
+
+        public object GetData(string name)
+        {
+            return CallContext.LogicalGetData(name);
+        }
+
+        public void SetData(string name, object data)
+        {
+            CallContext.LogicalSetData(name, data);
+        }
+    }
+}
