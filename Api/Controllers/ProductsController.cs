94b43067b10045bf8a09adb729ccb9bc6529b852
Grynet <nystad.fredrik@gmail.com>
2017-03-16 17:13:08 +0100

    ProductsController: removed unused using

--- Api/Controllers/ProductsController.cs
+++ Api/Controllers/ProductsController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 using WebApiTemplateProject.Api.Logic;
 using WebApiTemplateProject.Api.Models;
