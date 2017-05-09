72786190ccb44dc90f92ec2e5d0e06a0671a5168
Grynet <nystad.fredrik@gmail.com>
2017-05-09 20:21:44 +0200

    GlobalExceptionHandler now uses ExceptionToHttpResponse mapper

--- Utilities/ExceptionHandler/GlobalExceptionHandler.cs
+++ Utilities/ExceptionHandler/GlobalExceptionHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
@@ -6,6 +7,8 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.ExceptionHandling;
 using Microsoft.Rest;
+using WebApiTemplateProject.Utilities.Concurrency;
+using WebApiTemplateProject.Utilities.Mapper;
 
 namespace WebApiTemplateProject.Utilities.ExceptionHandler
 {
@@ -15,20 +18,22 @@ namespace WebApiTemplateProject.Utilities.ExceptionHandler
     /// </summary>
     public class GlobalExceptionHandler : System.Web.Http.ExceptionHandling.ExceptionHandler
     {
+        private readonly IExecutionContextValueProvider _executionContextValueProvider;
+
+        public GlobalExceptionHandler(IExecutionContextValueProvider executionContextValueProvider)
+        {
+            _executionContextValueProvider = executionContextValueProvider;
+        }
+
         public override Task HandleAsync(ExceptionHandlerContext context, CancellationToken cancellationToken)
         {
-            HttpStatusCode statusCode;
-            if(context.Exception is ArgumentException) statusCode = HttpStatusCode.BadRequest;          
-            else if (context.Exception is NotImplementedException) statusCode = HttpStatusCode.NotImplemented;
-            else if (context.Exception is HttpOperationException) statusCode = ((HttpOperationException) context.Exception).Response.StatusCode;
-            else statusCode = HttpStatusCode.InternalServerError;
+            var responseMessage = ExceptionToHttpResponse.ToHttpResponseMessage(context.Exception);
 
-            
             context.Result = new PlainTextErrorResult
             {
+                CorrelationIds = _executionContextValueProvider.GetCorrelationId(),
                 Request = context.Request,
-                ResponseMessage = context.Exception?.Message,
-                StatusCode = statusCode
+                ResponseMessage = responseMessage
             };
 
             return base.HandleAsync(context, cancellationToken);
@@ -36,20 +41,16 @@ namespace WebApiTemplateProject.Utilities.ExceptionHandler
 
         private class PlainTextErrorResult : IHttpActionResult
         {
+            public string CorrelationIds { private get; set; }
             public HttpRequestMessage Request { private get; set; }
-
-            public string ResponseMessage { private get; set; }
-
-            public HttpStatusCode StatusCode { private get; set; }
+            public HttpResponseMessage ResponseMessage { private get; set; }
 
             public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
             {
-                var response = new HttpResponseMessage(StatusCode)
-                {
-                    Content = new StringContent(ResponseMessage),
-                    RequestMessage = Request
-                };
-                return Task.FromResult(response);
+                ResponseMessage.Headers.Add("X-Correlation-ID", new List<string> { CorrelationIds });
+                ResponseMessage.RequestMessage = Request;
+
+                return Task.FromResult(ResponseMessage);
             }
         }
     }
