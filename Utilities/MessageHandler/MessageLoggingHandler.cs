6e76cd1114daa3b5eea29443ecc9bb8d193302ff
Grynet <nystad.fredrik@gmail.com>
2017-05-09 20:44:21 +0200

    Removed unused usings

--- Utilities/MessageHandler/MessageLoggingHandler.cs
+++ Utilities/MessageHandler/MessageLoggingHandler.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
-using System.IO;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using WebApiTemplateProject.Utilities.Concurrency;
