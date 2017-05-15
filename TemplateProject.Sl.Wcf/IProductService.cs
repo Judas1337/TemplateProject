c48614335078c11a63d9e13cb11acc019c38130a
Grynet <nystad.fredrik@gmail.com>
2017-05-15 16:19:19 +0200

    Added: Sl.Wcf

---
+++ TemplateProject.Sl.Wcf/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TemplateProject.Sl.Wcf
+{
+    [ServiceContract]
+    public interface IProductService
+    {
+        [OperationContract]
+        string GetData(int value);
+    }
+}
---
+++ TemplateProject.Sl.Wcf/Model/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace TemplateProject.Sl.Wcf.Model
+{
+    [DataContract]
+    public class Product
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Category { get; set; }
+        [DataMember]
+        public decimal Price { get; set; }
+        [DataMember]
+        public DateTimeOffset DateAdded { get; set; }
+    }
+}
