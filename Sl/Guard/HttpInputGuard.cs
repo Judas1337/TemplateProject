7ebf083f5f0d5be9eca1aa5ac329686d0369a539
Grynet <nystad.fredrik@gmail.com>
2017-05-15 10:21:42 +0200

    Sl.WebApi.Guard: Added HttpInputGuard

---
+++ Sl/Guard/HttpInputGuard.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TemplateProject.Utilities.Guard;
+
+namespace TemplateProject.Sl.WebApi.Guard
+{
+    /// <summary>
+    /// In WebApi, all uncaught exceptions except HttpResponseException are caught by the ExceptionLoggingHandler and ExceptionHandler.
+    /// If this is not the intention a HttpResponseException, which is the only exception who will not be caught should be thrown. 
+    /// </summary>
+    public static class HttpInputGuard
+    {
+        public static void ThrowBadRequestIfNull<TParam>(string parametername, TParam parameter)
+        {
+            ThrowHttpResponseExceptionIf(parametername, parameter, HttpStatusCode.BadRequest, (param) => param == null);
+        }
+
+        public static void ThrowBadRequestIfStringIsNullOrWhitespace(string parametername, string parameter)
+        {
+            ThrowHttpResponseExceptionIf(parametername, parameter, HttpStatusCode.BadRequest, (param) => string.IsNullOrWhiteSpace(param));
+        }
+
+        public static void ThrowBadRequestIfNegativeValue(string parametername, double parameter)
+        {
+            ThrowHttpResponseExceptionIf(parametername, parameter, HttpStatusCode.BadRequest, (param) => param < 0);
+        }
+
+        public static void ThrowBadRequestIfDefaultValue<TParam>(string parametername, TParam parameter)
+            where TParam : IEquatable<TParam>
+        {
+            ThrowHttpResponseExceptionIf(parametername, parameter, HttpStatusCode.BadRequest, (param) => param.Equals(default(TParam)));
+        }
+
+        public static void ThrowBadRequestIfNotDefaultValue<TParam>(string parametername, TParam parameter)
+        where TParam : IEquatable<TParam>
+        {
+            ThrowHttpResponseExceptionIf(parametername, parameter, HttpStatusCode.BadRequest, (param) => !param.Equals(default(TParam)));
+        }
+
+        public static void ThrowHttpResponseExceptionIf<TParam>(string parametername, TParam parameter, HttpStatusCode statusCode, Expression<Func<TParam, bool>> parameterValidator)
+        {
+            if (!GuardUtilities.ParamIsInvalid(parameter, parameterValidator)) return;
+            var exceptionMessage = GuardUtilities.GenerateExceptionMessage(parametername, parameter, parameterValidator);
+
+            var response = new HttpResponseMessage(statusCode)
+            {
+                Content = new StringContent(exceptionMessage)
+            };
+
+            throw new HttpResponseException(response);
+        }
+    }
+}
