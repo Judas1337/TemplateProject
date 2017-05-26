b7ccd9e61ac874ed1a5e7c7b326f27b5fc54f65f
Fredrik Nystad <nystad.fredrik@gmail.com>
2017-05-26 10:01:34 +0200

    Sl.Wcf: Added ProductService implementation

--- Sl/Wcf/IProductService.cs
+++ Sl/Wcf/IProductService.cs
@@ -1,4 +1,7 @@
-﻿using System.ServiceModel;
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using TemplateProject.Sl.Wcf.Model;
 
 namespace TemplateProject.Sl.Wcf
 {
@@ -6,6 +9,18 @@ namespace TemplateProject.Sl.Wcf
     public interface IProductService
     {
         [OperationContract]
-        string GetData(int value);
+        Task<IEnumerable<Product>> GetAllProducts();
+
+        [OperationContract]
+        Task<Product> GetProduct(int id);
+
+        [OperationContract]
+        Task<Product> CreateProduct(Product product);
+
+        [OperationContract]
+        Task<Product> UpdateProduct(Product product);
+
+        [OperationContract]
+        Task<Product> DeleteProduct(int id);
     }
 }
--- Sl/Wcf/ProductService.svc.cs
+++ Sl/Wcf/ProductService.svc.cs
@@ -1,12 +1,61 @@
-﻿namespace TemplateProject.Sl.Wcf
-{
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ProductService" in code, svc and config file together.
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TemplateProject.Bll.Contract.Bll.Interface;
+using TemplateProject.Sl.Wcf.Model;
+
+namespace TemplateProject.Sl.Wcf
+{    
     // NOTE: In order to launch WCF Test Client for testing this service, please select ProductService.svc or ProductService.svc.cs at the Solution Explorer and start debugging.
     public class ProductService : IProductService
     {
-        public string GetData(int value)
+        private readonly IProductLogic _productLogic;
+
+        public ProductService(IProductLogic productLogic)
         {
-            return string.Format("You entered: {0}", value);
+            _productLogic = productLogic;
+        }       
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            var domainModelProducts = await _productLogic.GetAllProducts();
+            var result = AutoMapper.Mapper.Map<IEnumerable<Product>>(domainModelProducts);
+
+            return result;
+        }        
+
+        public async Task<Product> GetProduct(int id)
+        {
+            var domainModelProduct = await _productLogic.GetProduct(id);
+            var result = AutoMapper.Mapper.Map<Product>(domainModelProduct);
+
+            return result;
+        }
+
+        public async Task<Product> CreateProduct(Product product)
+        {
+            var domainModelProduct = AutoMapper.Mapper.Map<Bll.Contract.Bll.Model.Product>(product);
+            domainModelProduct = await _productLogic.CreateProduct(domainModelProduct);
+            var result = AutoMapper.Mapper.Map<Product>(domainModelProduct);
+
+            return result;
+        }
+
+        public async Task<Product> UpdateProduct(Product product)
+        {
+            var domainModelProduct = AutoMapper.Mapper.Map<Bll.Contract.Bll.Model.Product>(product);
+            domainModelProduct = await _productLogic.UpdateProduct(domainModelProduct);
+            var result = AutoMapper.Mapper.Map<Product>(domainModelProduct);
+
+            return result;
+        }
+
+        public async Task<Product> DeleteProduct(int id)
+        {
+            var domainModelProduct = await _productLogic.DeleteProduct(id);
+            var result = AutoMapper.Mapper.Map<Product>(domainModelProduct);
+
+            return result;
         }
     }
 }
