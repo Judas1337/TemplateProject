c366e383fd47e8616e141e131bda94aa98a9f34e
Fredrik Nystad <nystad.fredrik@gmail.com>
2017-05-26 13:21:34 +0200

    IProductService: added faultcontracts

--- Sl/Wcf/IProductService.cs
+++ Sl/Wcf/IProductService.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ServiceModel;
 using System.Threading.Tasks;
-using TemplateProject.Bll.Contract.Bll.Model.Exceptions;
 using TemplateProject.Sl.Wcf.Model;
 using TemplateProject.Sl.Wcf.Model.Exceptions;
 
@@ -10,19 +9,33 @@ namespace TemplateProject.Sl.Wcf
     [ServiceContract]
     public interface IProductService
     {
-        [OperationContract]
+        [OperationContract]      
+        [FaultContract(typeof(InputFault))]
+        [FaultContract(typeof(GenericFault))]
         Task<IEnumerable<Product>> GetAllProducts();
 
-        [OperationContract]      
+        [OperationContract]     
+        [FaultContract(typeof(NotFoundFault))]
+        [FaultContract(typeof(InputFault))]
+        [FaultContract(typeof(GenericFault))]
         Task<Product> GetProduct(int id);
 
         [OperationContract]
+        [FaultContract(typeof(NotFoundFault))]
+        [FaultContract(typeof(InputFault))]
+        [FaultContract(typeof(GenericFault))]
         Task<Product> CreateProduct(Product product);
 
         [OperationContract]
+        [FaultContract(typeof(NotFoundFault))]
+        [FaultContract(typeof(InputFault))]
+        [FaultContract(typeof(GenericFault))]
         Task<Product> UpdateProduct(Product product);
 
         [OperationContract]
+        [FaultContract(typeof(NotFoundFault))]
+        [FaultContract(typeof(InputFault))]
+        [FaultContract(typeof(GenericFault))]
         Task<Product> DeleteProduct(int id);
     }
 }
