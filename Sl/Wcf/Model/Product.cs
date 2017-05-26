11db066b6852836ebef5313ec823c28e6e51ac0b
Fredrik Nystad <nystad.fredrik@gmail.com>
2017-05-26 11:12:30 +0200

    sl.wcf: product model updated, now datamembers are required

--- Sl/Wcf/Model/Product.cs
+++ Sl/Wcf/Model/Product.cs
@@ -6,15 +6,19 @@ namespace TemplateProject.Sl.Wcf.Model
     [DataContract]
     public class Product
     {
-        [DataMember]
+        [DataMember(IsRequired = true)]
         public int Id { get; set; }
-        [DataMember]
+
+        [DataMember(IsRequired = true)]
         public string Name { get; set; }
-        [DataMember]
+
+        [DataMember(IsRequired = true)]
         public string Category { get; set; }
-        [DataMember]
+
+        [DataMember(IsRequired = true)]
         public decimal Price { get; set; }
-        [DataMember]
+
+        [DataMember(IsRequired = true)]
         public DateTimeOffset DateAdded { get; set; }
     }
 }
