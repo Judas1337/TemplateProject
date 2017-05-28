aa7c6c5acf0badeaa14166d40282469ded294a39
Fredrik Nystad <nystad.fredrik@gmail.com>
2017-05-28 17:23:16 +0200

    ErrorHandlingAttribute refactor

--- Sl/Wcf/Attribute/ErrorHandingAttribute.cs
+++ Sl/Wcf/Attribute/ErrorHandingAttribute.cs
@@ -13,20 +13,20 @@ namespace TemplateProject.Sl.Wcf.Attribute
     public class ErrorHandlingAttribute : System.Attribute, IServiceBehavior
     {
         private readonly Type _errorHandlerType;
+        private readonly IErrorHandler _erroHandler;
 
         public ErrorHandlingAttribute(Type errorHandlingType)
         {
             _errorHandlerType = errorHandlingType;
+            _erroHandler = (IErrorHandler)Activator.CreateInstance(_errorHandlerType);
         }       
 
         public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
         {
-            IErrorHandler handler = (IErrorHandler)Activator.CreateInstance(_errorHandlerType);
-
             foreach (ChannelDispatcherBase chanDispBase in serviceHostBase.ChannelDispatchers)
             {
                 ChannelDispatcher channelDispatcher = chanDispBase as ChannelDispatcher;
-                if (channelDispatcher != null) channelDispatcher.ErrorHandlers.Add(handler);                
+                if (channelDispatcher != null) channelDispatcher.ErrorHandlers.Add(_erroHandler);                
             }
         }
 
