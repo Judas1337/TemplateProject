af55b88e01896f55ef118897c00125d06525fad8
Grynet <nystad.fredrik@gmail.com>
2017-05-31 10:41:38 +0200

    SemanticInputGuard added

---
+++ Bll/SemanticInputGuard.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq.Expressions;
+using TemplateProject.Bll.Contract.Bll.Model.Exceptions;
+using TemplateProject.Utilities.Guard;
+
+namespace TemplateProject.Bll
+{
+    /// <summary>
+    /// Used to perform input validations and generate instances of subclasses to <exception cref="SemanticException"></exception> upon validation failure.
+    /// </summary>
+    public class SemanticInputGuard
+    {
+        public static void ThrowInputExceptionIfNull<TParam>(string parametername, TParam parameter)
+        {
+            if (parameter == null) throw new ArgumentNullException(parametername);
+        }
+
+        public static void ThrowInputExceptionIfStringIsNullOrWhitespace(string parametername, string parameter)
+        {
+            GenericInputGuard.ThrowExceptionIfStringIsNullOrWhitespace<InputException>(parametername, parameter);
+        }
+
+        public static void ThrowInputExceptionIfNegativeValue(string parametername, double parameter)
+        {
+            GenericInputGuard.ThrowExceptionIfNegativeValue<InputException>(parametername, parameter);
+        }
+
+        public static void ThrowInputExceptionIfDefaultValue<TParam>(string parametername, TParam parameter)
+            where TParam : IEquatable<TParam>
+        {
+            GenericInputGuard.ThrowExceptionIfDefaultValue<TParam, InputException>(parametername, parameter);
+        }
+
+        public static void ThrowInputExceptionIfNotDefaultValue<TParam>(string parametername, TParam parameter)
+        where TParam : IEquatable<TParam>
+        {
+            GenericInputGuard.ThrowExceptionIfNotDefaultValue<TParam, InputException>(parametername, parameter);
+        }
+
+        public static void ThrowInputExceptionIf<TParam>(string parametername, TParam parameter, Expression<Func<TParam, bool>> parameterValidator)
+        {
+            GenericInputGuard.ThrowExceptionIf<TParam, InputException>(parametername, parameter, parameterValidator);
+        }
+    }
+}
